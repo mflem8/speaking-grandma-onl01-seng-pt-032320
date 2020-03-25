22c3940f6158abf3bbc5e47f22762e9723f4bf19
Matt Fleming <matthewfleming8@gmail.com>
2020-03-25 14:11:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,1 +1,9 @@
-def speak_to_grandma(phrase="hey grandma")
+def speak_to_grandma(phrase="Hey grandma")
+  if phrase = "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase = phrase.downcase
+    return "HUH?! SPEAK UP SONNY!"
+  elsif phrase = phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+end
