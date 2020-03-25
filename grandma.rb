fb728b277aa89d3acacd87a4b1a2d514e350b764
Matt Fleming <matthewfleming8@gmail.com>
2020-03-25 14:08:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,1 @@
 def speak_to_grandma(phrase="hey grandma")
-  if phrase == " phrase.downcase
-    return "HUH?! SPEAK UP SONNY!"
-  elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!" 
-    return "I LOVE YOU TOO PUMPKIN!"
-  end
-end
-
-def speak_to_grandma(phrase="hey grandma")
